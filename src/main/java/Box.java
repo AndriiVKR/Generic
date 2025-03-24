f7dcb80eb876849b211e6e152e728b6914b4b7b9
andrii.v <ustkos9@gmail.com>
2025-03-23 17:19:34 -0700

    New commit message

---
+++ src/main/java/Box.java
@@ -0,0 +1,18 @@
+public class Box {
+
+    private Object object;
+
+    public Box(Object object) {
+        this.object = object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+
+}
---
+++ src/test/java/BoxTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class BoxTest {
+
+    @Test
+    public void testBox() {
+        Box box1 = new Box(20);
+        Box box2 = new Box(10);
+        int expected = 30;
+        int result = (int) box1.getObject() + (int) box2.getObject();
+        assertEquals(expected, result);
+
+    }
+
+}
